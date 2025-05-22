d7fcc895271caba3c862995128c3a0df8fc7c37c
“Faith3494” <2279742213@qq.com>
2025-05-23 02:21:30 +0800

    前端首次提交

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+
+<template>
+  <div id="app">
+    <BasicLayout/>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+import BasicLayout from "@/layouts/BasicLayout.vue";
+import {useLoginUserStore} from "@/stores/useLoginUserStore.ts";
+
+
+const loginUserStore = useLoginUserStore()
+loginUserStore.fetchLoginUser()
+
+// import zhCN from 'ant-design-vue/es/locale/zh_CN';
+// import dayjs from 'dayjs';
+// import 'dayjs/locale/zh-cn';
+// dayjs.locale('zh-cn');
+
+</script>
+<style scoped>
+
+</style>
---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,92 @@
+<template>
+  <a-row :wrap="false">
+    <a-col flex="200px">
+      <RouterLink to="/">
+        <div class="title-bar">
+          <img class="logo" src="../assets/logo.png" alt="logo" />
+          <div class="title">云图库</div>
+        </div>
+      </RouterLink>
+    </a-col>
+    <a-col flex="auto">
+      <a-menu
+        v-model:selectedKeys="current"
+        mode="horizontal"
+        :items="items"
+        @click="doMenuClick"
+      />
+    </a-col>
+    <a-col flex="120px">
+      <div class="user-login-status">
+        <div v-if="loginUserStore.loginUser.id">
+          {{ loginUserStore.loginUser.userName ?? '无名' }}
+        </div>
+        <div v-else>
+          <a-button type="primary" href="/user/login">登录</a-button>
+        </div>
+      </div>
+
+    </a-col>
+  </a-row>
+</template>
+<script lang="ts" setup>
+import { h, ref } from 'vue'
+import { HomeOutlined } from '@ant-design/icons-vue'
+import { MenuProps } from 'ant-design-vue'
+import { useRouter } from 'vue-router'
+import {useLoginUserStore} from "@/stores/useLoginUserStore.ts";
+
+const current = ref<string[]>(['home'])
+const router = useRouter()
+
+const loginUserStore = useLoginUserStore()
+// 路由跳转
+const doMenuClick = ({ key }) => {
+  router.push({
+    path: key,
+  })
+}
+// 当前选中菜单
+// 监听路由变化，更新当前选中菜单
+router.afterEach((to, from, next) => {
+  current.value = [to.path];
+});
+
+
+
+const items = ref<MenuProps['items']>([
+  {
+    key: '/',
+    icon: () => h(HomeOutlined),
+    label: '主页',
+    title: '主页',
+  },
+  {
+    key: '/about',
+    label: '关于',
+    title: '关于',
+  },
+  {
+    key: 'other',
+    label: h('a', { href: 'https://www.baidu.com', target: '_blank' }, '棒棒的'),
+    title: '棒棒的',
+  },
+])
+</script>
+
+<style scoped>
+.title-bar {
+  display: flex;
+  align-items: center;
+}
+
+.title {
+  color: black;
+  font-size: 18px;
+  margin-left: 16px;
+}
+
+.logo {
+  height: 48px;
+}
+</style>
